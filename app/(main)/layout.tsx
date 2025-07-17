60e2c6b95dba25ed7b3f49b28a6690f977b66d79
arfanroky <arfanroky0@gmail.com>
2025-07-17 18:27:04 +0600

    feat: add blog and about pages with dynamic content and layout structure

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import type React from "react";
+import { Header } from "@/components/layout/header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
